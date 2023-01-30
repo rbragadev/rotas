8cba42f549c7dcc7c1207549c8b4175ea50f25b1
Raphael Braga <raffael.info@gmail.com>
2023-01-30 08:57:43 -0300

    aula 54 - forma2 de pegar dados da rota

--- src/app/curso-detalhe/curso-detalhe.component.ts
+++ src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css'],
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit {
   id!: string;
+  inscricao!: Subscription;
 
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id'];
+    // Aula 53 : Forma 1 de pegar dados da rota
+    //this.id = this.route.snapshot.params['id'];
+    //console.log(this.route)
+  }
+
+  ngOnInit(): void {
+    // Aula 54: Forma 2 de pegar dados da rota
+    this.inscricao = this.route.params.subscribe((params: any) => {
+      this.id = params['id'];
+    });
+  }
+
+  // Aula 54: Forma 2 de pegar dados da rota
+  ngOnDestroy() {
+    this.inscricao.unsubscribe();
   }
 }
