4faa2c034428b8088d848aa6965d61ae428f33b9
Raphael Braga <raffael.info@gmail.com>
2023-01-31 15:49:55 -0300

    aula 60 - rotas filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AlunosModule } from './alunos/alunos.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CursosModule } from './cursos/cursos.module';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+
 //import { CursosComponent } from './cursos/cursos.component';
 //import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
 //import { CursosService } from './cursos/cursos.service';
@@ -16,11 +18,12 @@ import { LoginComponent } from './login/login.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
+
     // CursosComponent,
     // CursoDetalheComponent,
     // NaoEncontradoComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, CursosModule],
+  imports: [BrowserModule, AppRoutingModule, CursosModule, AlunosModule],
   //providers: [CursosService],
   bootstrap: [AppComponent],
 })
